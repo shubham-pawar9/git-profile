9cbd870e3bcaead467eac7216f22cfc8d8b9ee80
shubham-pawar9 <developer.shubhampawar@gmail.com>
2023-11-27 09:49:33 +0530

    start on UI

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Cover from "./component/Cover";
+
+function App() {
+  return <Cover />;
+}
+
+export default App;
---
+++ src/component/Cover.js
@@ -0,0 +1,20 @@
+const Cover = () => {
+  return (
+    <>
+      <div className="cover-div">
+        <div className="options-div">
+          <div className="one">One</div>
+          <div className="two">Two</div>
+          <div className="three">Three</div>
+          <div className="four">Four</div>
+          <div className="five">Five</div>
+          <div className="six">Six</div>
+          <div className="seven">Seven</div>
+          <div className="eight">Eight</div>
+        </div>
+        <div className="middle-circle"></div>
+      </div>
+    </>
+  );
+};
+export default Cover;
