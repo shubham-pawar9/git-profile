f2c46ab1dce2df5369a334e343f9cfb8117ca908
shubham-pawar9 <developer.shubhampawar@gmail.com>
2023-11-29 10:36:33 +0530

    update json

--- src/component/Cover.js
+++ src/component/Cover.js
@@ -1,16 +1,62 @@
+import { useEffect, useState } from "react";
+
 const Cover = () => {
+  const [apiData, setApiData] = useState([]);
+  const fetchRepos = () => {
+    return new Promise(async (resolve, reject) => {
+      try {
+        const url = await fetch(
+          "https://api.github.com/users/shubham-pawar9/repos"
+        );
+        const urlData = url.json();
+        resolve(urlData);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  };
+  useEffect(() => {
+    fetchRepos()
+      .then((res) =>
+        setApiData(() =>
+          res.filter((item) => {
+            return [
+              "elevator-2-lifts",
+              "ai-tool-shunu",
+              "employee-task-list",
+              "news-application",
+              "piano",
+              "shopping-cart",
+              "virtual-smart-watch",
+              "my-portfolio",
+            ].includes(item.name);
+          })
+        )
+      )
+      .catch((err) => console.error(err));
+  }, []);
+  const handleRepoEvent = (details) => {
+    console.log(details);
+  };
+
   return (
     <>
       <div className="cover-div">
         <div className="options-div">
-          <div className="one">One</div>
-          <div className="two">Two</div>
-          <div className="three">Three</div>
-          <div className="four">Four</div>
-          <div className="five">Five</div>
-          <div className="six">Six</div>
-          <div className="seven">Seven</div>
-          <div className="eight">Eight</div>
+          {apiData &&
+            apiData.map((item, index) => {
+              return (
+                <div
+                  key={index}
+                  className={`circle${index + 1}`}
+                  onClick={() => handleRepoEvent(item)}
+                >
+                  <img
+                    src={process.env.PUBLIC_URL + `/images/${item.name}.png`}
+                  />
+                </div>
+              );
+            })}
         </div>
         <div className="middle-circle"></div>
       </div>
